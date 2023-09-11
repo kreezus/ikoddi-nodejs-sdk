3efdde6277c5c6168c78ad4aa11da9fe0d5ba78a
Rachid <ouattchidi@gmail.com>
2023-09-11 11:51:24 +0200

    Add Otp functions

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,67 @@
+export declare const SMSStatus: {
+    SendingOK: string;
+    SendingOKNoReport: string;
+    SendingError: string;
+    DeliveryOK: string;
+    DeliveryFailed: string;
+    DeliveryPending: string;
+    DeliveryUnknown: string;
+    Error: string;
+};
+export declare type SMSStatus = (typeof SMSStatus)[keyof typeof SMSStatus];
+export declare const SMSSMSCStatus: {
+    DeliverySuccess: string;
+    DeliveryFailure: string;
+    MessageBuffered: string;
+    SmscSubmit: string;
+    SmscReject: string;
+};
+export declare type SMSSMSCStatus = (typeof SMSSMSCStatus)[keyof typeof SMSSMSCStatus];
+export declare type SMS = {
+    id: string;
+    to: string;
+    message: string;
+    from: string;
+    cost: number;
+    smsAccountId: string;
+    status: SMSStatus;
+    smscStatus: SMSSMSCStatus | null;
+    smsBroadCast: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+};
+export declare type OTPResponse = {
+    status: 0 | -1;
+    otpToken: string;
+};
+export declare type VerifyOPTRequest = {
+    identity: string;
+    otp: string;
+    verificationKey: string;
+};
+export declare type VerifyOPTResponse = {
+    identity: string;
+    otp: string;
+    verificationKey: string;
+};
 export declare class Ikoddi {
     private apiKey;
     private apiBaseURL;
     private groupId;
+    private otpAppId;
     constructor();
     withApiKey(apiKey: string): Ikoddi;
     withApiBaseURL(apiBaseURL: string): Ikoddi;
     withGroupId(groupId: string): Ikoddi;
+    withOtpAppId(otpAppId: string): Ikoddi;
     _assertAllParametersAreCorrect(): void;
-    sendAirtime(numbers: Array<string>, ref: string, amount: string, campaignName: string, phonecode?: string, isoCode?: string): Promise<import("axios").AxiosResponse<any, any>>;
-    sendSMS(numbers: Array<string>, from: string, message: string, smsBroadCast: string, phonecode?: string, isoCode?: string): Promise<import("axios").AxiosResponse<any, any>>;
-    internetPlans(): Promise<import("axios").AxiosResponse<any, any>>;
+    sendAirtime(numbers: Array<string>, ref: string, amount: string, campaignName: string, phonecode?: string, isoCode?: string): Promise<any>;
+    sendSMS(numbers: Array<string>, from: string, message: string, smsBroadCast: string, phonecode?: string, isoCode?: string): Promise<SMS[]>;
+    sendOTP(identity: string, type?: "SMS" | "EMAIL"): Promise<OTPResponse>;
+    verifyOTP(otpData: {
+        verificationKey: string;
+        otp: string;
+        identity: string;
+    }): Promise<OTPResponse>;
+    internetPlans(): Promise<any>;
 }
